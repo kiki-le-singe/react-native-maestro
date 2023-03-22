feea1491fda819673f8f3ca3178d6c8a699e3ac3
anthony <aalbertini95@gmail.com>
2023-03-22 15:10:27 +0100

    Update SignIn screen design/styles

--- app/(auth)/sign-in-modal.js
+++ app/(auth)/sign-in-modal.js
@@ -1,9 +1,13 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
+
 import { useAuth } from "../../context/auth";
 import colors from "../../utils/colors";
 
 export default function SignIn() {
   const { signIn } = useAuth();
+  const [text, onChangeText] = React.useState("");
+  const [number, onChangeNumber] = React.useState("");
 
   function handleSignIn() {
     signIn();
@@ -11,7 +15,25 @@ export default function SignIn() {
 
   return (
     <View style={styles.container}>
-      <Text onPress={handleSignIn}>Sign In</Text>
+      <Text style={styles.titleScreen}>Hello!</Text>
+
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeText}
+        value={text}
+        placeholder="Email"
+        placeholderTextColor={colors.BLUE}
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Password"
+        placeholderTextColor={colors.BLUE}
+        secureTextEntry
+      />
+
+      <Button title="Sign In" color={colors.GREEN} onPress={handleSignIn} />
     </View>
   );
 }
@@ -22,5 +44,21 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: colors.BLACK,
+  },
+  titleScreen: {
+    fontSize: 34,
+    color: colors.WHITE,
+    marginBottom: 10,
+  },
+  input: {
+    width: 200,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    borderColor: colors.BLUE,
+    borderRadius: 8,
+    padding: 10,
+    color: colors.WHITE,
+    fontWeight: "bold",
   },
 });
