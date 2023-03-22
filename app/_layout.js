b9f07f6242735c74477eab59c97b604ba39856e1
anthony <aalbertini95@gmail.com>
2023-03-22 11:28:24 +0100

    Update navigation of the app

--- app/_layout.js
+++ app/_layout.js
@@ -4,23 +4,30 @@ import { Provider } from "../context/auth";
 export default function Root() {
   return (
     <Provider>
-      <Stack>
-        <Stack.Screen
-          name="index"
-          options={{
-            headerShown: false,
-          }}
-        />
+      <Stack
+        initialRouteName="index"
+        screenOptions={({ route, navigation }) => ({
+          headerShown: false,
+          gestureEnabled: false,
+        })}
+      >
+        <Stack.Screen name="index" />
         <Stack.Screen
           name="details"
           options={{
+            headerShadowVisible: true,
+            headerStyle: {
+              backgroundColor: "rgba(51, 51, 51, 1)",
+            },
+            headerTitleStyle: { color: "white" },
+            headerTintColor: "rgba(13, 180, 185, 1)",
             title: "Details",
+            headerShown: true,
           }}
         />
         <Stack.Screen
           name="(auth)/sign-in-modal"
           options={{
-            headerShown: false,
             presentation: "modal",
           }}
         />
