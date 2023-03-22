d56351c551bc1b857ad741b00c487526d1502a45
anthony <aalbertini95@gmail.com>
2023-03-22 19:42:09 +0100

    Extract form ini func and add shaking animation on errors

--- app/(auth)/sign-in-modal.js
+++ app/(auth)/sign-in-modal.js
@@ -2,7 +2,6 @@ import React from "react";
 import {
   StyleSheet,
   Text,
-  View,
   TextInput,
   Button,
   KeyboardAvoidingView,
@@ -10,11 +9,20 @@ import {
   Platform,
   Keyboard,
 } from "react-native";
-import { Formik, Form, Field, ErrorMessage } from "formik";
+import { Formik } from "formik";
 import { object, string } from "yup";
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  withRepeat,
+  withSequence,
+  withTiming,
+} from "react-native-reanimated";
 
 import { useAuth } from "../../context/auth";
 import colors from "../../utils/colors";
+
+const SHAKING_DISTANCE = 10;
 
 const signInValidationSchema = object({
   email: string()
@@ -29,12 +37,80 @@ const signInValidationSchema = object({
 });
 
 export default function SignIn() {
+  const position = useSharedValue(0);
   const { signIn } = useAuth();
+
+  const startAnimation = () => {
+    position.value = withRepeat(
+      withSequence(
+        withTiming(-SHAKING_DISTANCE, { duration: 50 }),
+        withTiming(SHAKING_DISTANCE, { duration: 50 }),
+        withTiming(0, { duration: 50 })
+      ),
+      2,
+      true
+    );
+  };
+
+  const animatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{ translateX: position.value }],
+    };
+  });
 
   function onSubmit(values) {
     signIn(values);
     console.log(values);
   }
+
+  const renderForm = ({
+    handleChange,
+    handleSubmit,
+    values,
+    errors,
+    touched,
+  }) => (
+    <>
+      <TextInput
+        style={styles.input(errors.email && touched.email)}
+        onChangeText={handleChange("email")}
+        value={values.email}
+        placeholder="Email"
+        placeholderTextColor={colors.BLUE}
+        autoCapitalize="none"
+        autoComplete="off"
+      />
+      <TextInput
+        style={styles.input(errors.password && touched.password)}
+        onChangeText={handleChange("password")}
+        value={values.password}
+        placeholder="Password"
+        placeholderTextColor={colors.BLUE}
+        autoCapitalize="none"
+        autoComplete="off"
+        secureTextEntry
+      />
+      <Button
+        title="Sign In"
+        color={colors.GREEN}
+        onPress={(e) => {
+          if (!values.email || !values.password) {
+            startAnimation();
+          }
+
+          handleSubmit(e);
+        }}
+      />
+
+      {errors.email && touched.email ? (
+        <Text style={styles.error}>{errors.email}</Text>
+      ) : null}
+
+      {errors.password && touched.password ? (
+        <Text style={styles.error}>{errors.password}</Text>
+      ) : null}
+    </>
+  );
 
   return (
     <KeyboardAvoidingView
@@ -42,7 +118,7 @@ export default function SignIn() {
       style={styles.container}
     >
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-        <View style={styles.inner}>
+        <Animated.View style={[styles.inner, animatedStyle]}>
           <Text style={styles.titleScreen}>Hello!</Text>
 
           <Formik
@@ -50,44 +126,9 @@ export default function SignIn() {
             validationSchema={signInValidationSchema}
             onSubmit={onSubmit}
           >
-            {({ handleChange, handleSubmit, values, errors, touched }) => (
-              <>
-                <TextInput
-                  style={styles.input(errors.email && touched.email)}
-                  onChangeText={handleChange("email")}
-                  value={values.email}
-                  placeholder="Email"
-                  placeholderTextColor={colors.BLUE}
-                  autoCapitalize="none"
-                  autoComplete="off"
-                />
-                <TextInput
-                  style={styles.input(errors.password && touched.password)}
-                  onChangeText={handleChange("password")}
-                  value={values.password}
-                  placeholder="Password"
-                  placeholderTextColor={colors.BLUE}
-                  autoCapitalize="none"
-                  autoComplete="off"
-                  secureTextEntry
-                />
-                <Button
-                  title="Sign In"
-                  color={colors.GREEN}
-                  onPress={handleSubmit}
-                />
-
-                {errors.email && touched.email ? (
-                  <Text style={styles.error}>{errors.email}</Text>
-                ) : null}
-
-                {errors.password && touched.password ? (
-                  <Text style={styles.error}>{errors.password}</Text>
-                ) : null}
-              </>
-            )}
+            {renderForm}
           </Formik>
-        </View>
+        </Animated.View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
   );
